c0fa5b8ee2556a3db5f65bad65ca218969c4e3e3
Agustín Aranda Cruz <66988375+Agustin-Aranda@users.noreply.github.com>
2025-03-12 16:01:28 -0600

    Connection to PostgreSQL with AWS
    Making the connection to PostgreSQL using an instance of AWS and DBeaver as a IDE

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,7 +1,54 @@
 package com.revature;
 
+import com.revature.models.Role;
+import com.revature.models.User;
+import com.revature.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Connection conn = ConnectionUtil.getConnection();
+        String sql = "SELECT * FROM usuarios;";
+        List<User> allUsers = new ArrayList<>();
+
+        try {
+            Statement stmt = conn.createStatement();
+
+            ResultSet res = stmt.executeQuery(sql);
+
+            while (res.next()) {
+
+                User u = new User();
+                u.setUserId(res.getInt("user_id"));
+                u.setFirstName(res.getString("first_name"));
+                u.setLastName(res.getString("last_name"));
+                u.setEmail(res.getString("email"));
+                u.setPassword(res.getString("password"));
+                u.setRole(Role.valueOf(res.getString("role")));
+
+                allUsers.add(u);
+            }
+
+            for (User u : allUsers) {
+                System.out.println("ID: " + u.getUserId() +
+                        ", FirstName: " + u.getFirstName() +
+                        ", LastName: " + u.getLastName() +
+                        ", Email: " + u.getEmail() +
+                        ", Rol: " + u.getRole());
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Could not get all the users!");
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,70 @@
+package com.revature.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    //Private Static instance
+    private static Connection conn = null;
+    //Private constructor
+    private  ConnectionUtil(){
+
+    }
+
+    //public static getInstance method
+
+    public static  Connection getConnection(){
+        //If a connection already exists. We return it
+        try {
+            if(conn != null && !conn.isClosed()){
+                return conn;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        //Otherwise we create a new connection
+        //To create the connection we need
+        //Database connection
+        //Database UserName
+        //Database password
+
+        String url = "";
+        String username = "";
+        String password = "";
+
+        Properties props = new Properties();
+
+        try {
+            props.load(new FileReader("src/main/resources/application.properties"));
+            url = props.getProperty("url");
+            username = props.getProperty("username");
+            password = props.getProperty("password");
+
+            conn = DriverManager.getConnection(url,username, password);
+            System.out.println("Connected to the database");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Could not connect to the database");
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Could not connect to the database");
+
+        }catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Could not connect to the database");
+
+        }
+
+        return conn;
+
+    }
+}
